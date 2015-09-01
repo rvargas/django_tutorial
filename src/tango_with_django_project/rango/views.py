83a21189a65fcff07e3e8b9cef1daee18e5ce33f
Rafael Vargas <rv@manoderecha.mx>
2015-08-31 19:37:33 -0500

    7. Models, Templates and Views

--- src/tango_with_django_project/rango/views.py
+++ src/tango_with_django_project/rango/views.py
@@ -2,11 +2,33 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
 
+from rango.models import Category, Page
+
 
 def index(request):
-    context_dict = {'boldmessage': 'I am bold'}
+    category_list = Category.objects.order_by('-likes')[:5]
+    pages_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories':category_list, 'pages':pages_list}
+
     return render(request, 'rango/index.html', context_dict)
 
 
 def about(request):
     return render(request, 'rango/about.html', {})
+
+
+def get_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        pass
+
+    return render(request, 'rango/category.html', context_dict)
