731f7a9bad470ca041cd8124b49589336cf6b5ab
Rafael Vargas <rv@manoderecha.mx>
2015-09-04 14:50:57 -0500

    8. Fun with Forms

--- src/tango_with_django_project/rango/urls.py
+++ src/tango_with_django_project/rango/urls.py
@@ -1,8 +1,14 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from rango import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
                        url(r'^about$', views.about, name='about'),
-                       url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.get_category, name='category'),
+                       url(r'^add_category/$', views.add_category,
+                           name='add_category'),
+                       url(r'^add_page/(?P<category_name_slug>[\w\-]+)/$',
+                           views.add_page,
+                           name='add_page'),
+                       url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+                           views.get_category, name='category'),
                        )
